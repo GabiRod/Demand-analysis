bd53718b12dd55dc82f7ab76ead20d2ff802a40d
HansenPeter <psh@nozebra.dk>
2020-04-07 12:41:30 +0200

    Create DiscoveryController.cs
    for viewing endpoints on /api/

---
+++ back-end/Controllers/DiscoveryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class DiscoveryController : ControllerBase
+    {
+        // GET: api/Discovery
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "/WeatherForecast", "/GSC" };
+        }
+
+    }
+}
