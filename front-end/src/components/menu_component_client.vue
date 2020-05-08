95d15b039713c94a27f440d9a59b1265c62b030c
Gabriela-Rod <59563039+Gabriela-Rod@users.noreply.github.com>
2020-05-08 10:02:13 +0200

    Update menu_component_client.vue

--- front-end/src/components/menu_component_client.vue
+++ front-end/src/components/menu_component_client.vue
@@ -9,7 +9,7 @@
       <div class="menu_bar">
         <img class="menu_logo" src="../assets/resize.png" alt="" />
         <router-link to="/client_dashboard_page">back</router-link>
-        <h1 class="menu_title">{{ info }}</h1>
+        <h1 class="menu_title">{{ info.data }}</h1>
       </div>
 
       <button class="logout_button" v-on:click="logout()">LOG OUT</button>
